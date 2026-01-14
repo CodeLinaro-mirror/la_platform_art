c7cb3d7ba90c26df19f37cd96b45cf74fac8383c
Santosh Kumar Satla <ssatla@qti.qualcomm.com>
2026-01-14 02:08:38 -0800

    Revert "Don't forget arrays of `java.lang.Object` when updating image classes."
    This reverts commit 341238adda58a2333cffc15540aa5a4456ed5913.
    
    Change-Id: Ie7cc950ac665a1b915e3145286f03fa742f46ac3

--- dex2oat/driver/compiler_driver.cc
+++ dex2oat/driver/compiler_driver.cc
@@ -1419,10 +1419,6 @@ static void MaybeAddToImageClasses(Thread* self,
     }
     klass = klass->GetSuperClass();
   }
-  if (klass->IsObjectClass() && array_dim > 0) {
-    TypeReference type_ref(&klass->GetDexFile(), klass->GetDexTypeIndex());
-    image_classes->Add(type_ref, array_dim);
-  }
 }
 
 // Keeps all the data for the update together. Also doubles as the reference visitor.
